cefee09fb3c1958f54c3cb11c08bfd442badbfb2
tankerz <sambagutancredi@protonmail.com>
2024-05-01 15:28:48 +1000

    human and computer input basics

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 console.log("hello, world nigga");
 
 function getComputerChoice(min, max) {
+  min = 1;
+  max = 4;
   return Math.floor(Math.random() * (max - min) + min);
 }
-console.log(getComputerChoice(1, 4));
 
+let question = prompt("rock, paper,scisors?");
 function getHumanChoice() {
-  prompt("rock, paper,scisors?");
+  if (question.toLowerCase() === "scissors") return "scissors";
+  else if (question.toLowerCase() === "rock") return "rock";
+  else if (question.toLowerCase() === "paper") return "paper";
 }
 
 console.log(getHumanChoice());
